9da5a1fdf2723e240572b336d18e6c267889433c
Tanay Kothari <tanaykothari1998@gmail.cm>
2017-02-07 20:59:23 -0800

    Fixing db access

--- src/db_setup.py
+++ src/db_setup.py
@@ -3,9 +3,9 @@ from app import TodoItem, User, db
 db.drop_all()
 db.create_all()
 
-admin = User(username='admin')
-todo1 = TodoItem(text='Eat cookie', user=admin, date=datetime.utcnow(), dateCompleted=datetime.utcnow())
-todo2 = TodoItem(text='Wash clothes', user=admin, date=datetime.utcnow(), dateCompleted=None)
+admin = User(sender_id='admin')
+todo1 = TodoItem(text='Eat cookie', user=admin, dateAdded=datetime.utcnow(), dateCompleted=datetime.utcnow())
+todo2 = TodoItem(text='Wash clothes', user=admin, dateAdded=datetime.utcnow(), dateCompleted=None)
 
 db.session.add(admin)  # This will also add admin_address to the session.
 db.session.add(todo1)  # This will also add guest_address to the session.
